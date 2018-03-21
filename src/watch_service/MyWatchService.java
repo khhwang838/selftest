54d04401e526f09b7def19a7e1e0c5142ac33c4a
Kihyun Hwang <kihyun.hwang@m2utech.com>
2018-03-21 09:28:36 +0900

    파일수정 시 시작,종료 이벤트가 발생하며  시작시에는 프로세스 처리가 발생하지 않도록 수정

--- src/watch_service/MyWatchService.java
+++ src/watch_service/MyWatchService.java
@@ -19,24 +19,36 @@ public class MyWatchService {
 		String configDirPath = configFilePath.substring(0, lastIndexOfSeparator);
 		System.out.println(configDirPath);
 		
-		Path path = Paths.get(configFilePath);
+		Path path = Paths.get(configDirPath);
 		WatchService watchService = path.getFileSystem().newWatchService();
 		path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+		boolean startModifyFile = true;
 		for(;;) {
 			WatchKey key = watchService.take();
 			for (WatchEvent<?> event : key.pollEvents()) {
-				if ( event.kind() == StandardWatchEventKinds.ENTRY_MODIFY ) {
+				System.out.println(event.count() + event.kind().name());
+				// 파일명 출력
+				System.out.println(event.context().toString());
+				
+				if ( StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind()) && startModifyFile) {
+					startModifyFile = false;
+					continue;
+				}
+				if ( event.count()== 1 && StandardWatchEventKinds.ENTRY_MODIFY.equals(event.kind()) ) {
 					// 설정값 다시 읽어오기
 					System.out.println("Reloading config values....");
-					
+					startModifyFile = true;
+				} else {
+					System.out.println("Not modification event.");
 				}
 			}
 			
 			boolean isValid = key.reset();
 			if (!isValid) {
-				// file no longer regiestered(doesn't exist)
+				// file no longer regiestered
 				System.out.println("Config file does not exist...");
 			}
+			System.out.println("================================");
 			Thread.sleep(5000);
 		}
 	}
