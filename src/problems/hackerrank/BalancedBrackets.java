ab7aeab79a3de0d432a0a88b2a5097eb265190a2
Kihyun Hwang <kihyun.hwang@m2utech.com>
2018-04-25 13:47:20 +0900

    해커랭크 문제 주석 추가

--- src/problems/hackerrank/BalancedBrackets.java
+++ src/problems/hackerrank/BalancedBrackets.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 /**
  * Hackerrank.com
  * Stacks: Balanced Brackets
- * 
+ * 괄호가 제대로 닫혀있는지 검사하는 로직
  * @author Kihyun
  * @created   2018-02-15
  */
@@ -27,23 +27,23 @@ public class BalancedBrackets {
 		
 		if ( expression.length() % 2 > 0 ) return false;
 		
-		Stack<Character> s = new Stack<>();
+		Stack<Character> stack = new Stack<>();
         
 		for (int idx = 0; idx < expression.length() ; idx++ ) {
         	Character c = expression.charAt(idx);
         	if ( c == '(' || c == '{' || c == '[') {
-        		s.push(c);
+        		stack.push(c);
         	} else {
-        		if (s.isEmpty() ) {
+        		if (stack.isEmpty() ) {
         			return false;
         		} else {
-	        		Character p = s.pop();
+	        		Character p = stack.pop();
 	        		if ( (c == ')' && p != '(') || (c == '}' && p != '{') || (c == ']' && p != '[') ){
 		        		return false;
 		        	}
         		}
         	}
         }
-        return s.isEmpty();		//  <-- return true; 가 아니라 스택이 비었는지를 확인해야 함
+        return stack.isEmpty();		//  <-- return true; 가 아니라 스택이 비었는지를 확인해야 함
     }
 }
