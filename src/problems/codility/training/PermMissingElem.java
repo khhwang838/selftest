61302adf19654a00e75207a72c10423026bbbfa4
Kihyun Hwang <kihyun.hwang@m2utech.com>
2018-04-30 14:58:37 +0900

    코딜리티 문제풀이 추가

---
+++ src/problems/codility/training/PermMissingElem.java
@@ -0,0 +1,59 @@
+package problems.codility.training;
+
+/**
+ * N개의 distinct한 숫자를 갖는 배열 A를 input으로 받아 missing 숫자를 리턴하기.
+ * 배열A에는 1 ~ (N+1) 까지의 숫자가 들어있을 수 있음
+ * 
+ * An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+ * 
+ * Your goal is to find that missing element.
+ * 
+ * Write a function:
+ * 
+ * int solution(int A[], int N);
+ * 
+ * that, given an array A, returns the value of the missing element.
+ * 
+ * For example, given array A such that:
+ * 
+ *   A[0] = 2
+ *   A[1] = 3
+ *   A[2] = 1
+ *   A[3] = 5
+ * the function should return 4, as it is the missing element.
+ * 
+ * Assume that:
+ * 
+ * N is an integer within the range [0..100,000];
+ * the elements of A are all distinct;
+ * each element of array A is an integer within the range [1..(N + 1)].
+ * Complexity:
+ * 
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+ * 
+ * @author Kihyun
+ * @created   2018-04-28
+ */
+public class PermMissingElem {
+
+	/*
+	 * 1부터 N+1까지의 합을 구한 뒤 배열의 숫자를 차례로 빼서 마지막에 남는 수가 결과임
+	 */
+	public static int solution(int[] A) {
+        // write your code in Java SE 8
+		long N = A.length +1;
+        long totalSum = (N*(N+1))/2;
+        
+        for(int i=0; i<A.length; i++){
+            totalSum -= A[i];
+        }
+        
+        return (int)totalSum;
+    }
+	
+	public static void main(String[] args) {
+		int[] A = {};
+		System.out.println(solution(A));
+	}
+}
---
+++ src/problems/codility/training/TapeEquilibrium.java
@@ -0,0 +1,82 @@
+package problems.codility.training;
+
+/**
+ * A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+ * 
+ * Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+ * 
+ * The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+ * 
+ * In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+ * 
+ * For example, consider array A such that:
+ * 
+ *   A[0] = 3
+ *   A[1] = 1
+ *   A[2] = 2
+ *   A[3] = 4
+ *   A[4] = 3
+ * We can split this tape in four places:
+ * 
+ * P = 1, difference = |3 − 10| = 7 
+ * P = 2, difference = |4 − 9| = 5 
+ * P = 3, difference = |6 − 7| = 1 
+ * P = 4, difference = |10 − 3| = 7 
+ * Write a function:
+ * 
+ * class Solution { public int solution(int[] A); }
+ * 
+ * that, given a non-empty array A of N integers, returns the minimal difference that can be achieved.
+ * 
+ * For example, given:
+ * 
+ *   A[0] = 3
+ *   A[1] = 1
+ *   A[2] = 2
+ *   A[3] = 4
+ *   A[4] = 3
+ * the function should return 1, as explained above.
+ * 
+ * Assume that:
+ * 
+ * N is an integer within the range [2..100,000];
+ * each element of array A is an integer within the range [−1,000..1,000].
+ * Complexity:
+ * 
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+ * 
+ * @author Kihyun
+ * @created   2018-04-30
+ */
+public class TapeEquilibrium {
+	public static int solution(int[] A) {
+        // write your code in Java SE 8
+		
+		int totalSum = 0;
+		for ( int a : A ) {
+			totalSum += a;
+		}
+		
+		int leftSum = 0;
+		
+		int minDiff = Integer.MAX_VALUE;
+		for ( int idx = 0; idx < A.length - 1 ; idx++ ) {
+			leftSum += A[idx];
+			int rightSum = totalSum - leftSum;
+			System.out.println("rSum: " + rightSum);
+			int diff = Math.abs(rightSum - leftSum);
+			if ( diff < minDiff ) {
+				minDiff = diff;
+			}
+		}
+		System.out.println("minDiff: " + minDiff);
+		return minDiff;
+    }
+	
+	public static void main(String[] args) {
+//		int[] A = {3, 1, 2, 4, 3};
+		int[] A = {5, -6, 2, 100, 34};
+		System.out.println("result: " + solution(A));
+	}
+}
